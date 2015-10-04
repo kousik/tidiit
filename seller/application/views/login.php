1fabc020a295ef48466ea69433899ca59bc2fc80
cto-tidiit <cto.tidiit@gmail.com>
2015-10-04 16:36:03 +0530

    seller login page interface change

--- seller/application/views/login.php
+++ seller/application/views/login.php
@@ -43,34 +43,124 @@
             <form id="registration_form" name="registration_form">
               <p class="text-muted text-center">Registration</p>
           <div class="col-md-6">
-                <input type="text" name="userName" id="userName" placeholder="userName" class="form-control top" required>
-                <input type="password" name="password" id="password" placeholder="Password" class="form-control middle" required>
+                <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="userName" name="userName" type="email" placeholder="enter your email for login" class="form-control email" required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group  col-md-8"> 
+                      <input id="password" name="password" type="password" placeholder="enter your  password" class="form-control" required >
+                      <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group  col-md-8"> 
+                      <input id="confirmPassword" name="confirmPassword" type="password" placeholder="enter your  confirm password" class="form-control" required >
+                      <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                        <input id="firstName" name="firstName" type="text" placeholder="ener seller first name" class="form-control" required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                        <input id="lastName" name="lastName" type="text" placeholder="ener seller last name" class="form-control" required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="email" name="email" type="email" placeholder="enter your email" class="form-control email" required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="mobile" name="mobile" type="phone" placeholder="enter your mobile no" class="form-control " required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+                <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <canvas id="secret_img" width="110" height="25" style="border:1px solid #d3d3d3;padding-top: 8px; padding-left:5px;float: left;"><br /> Browser Not Support HTML5.</canvas><p style="font-size:12px;font-weight: bold;float: left;padding:10px 0 0 8px;">Not getting it ?<a href="javascript:void(0);" onclick="myJsMain.commonFunction.GeneratNewImage();">Click here</a>  to get a new.</p> 
+                    </div>
+                </div>
+                <!--<input type="text" name="userName" id="userName" placeholder="userName" class="form-control top" required>
+                <input type="password" name="password" id="password" placeholder="Password" class="form-control middle" required> 
                 <input type="password" name="confirmPassword"  id="confirmPassword" placeholder="Re-password" class="form-control middle" required>            
             <input type="text" name="firstName" id="firstName" placeholder="First Name" class="form-control middle" required>
             <input type="text"  name="lastName" id="lastName" placeholder="Last Name" class="form-control middle" required>
             <input type="email" name="email" id="email" placeholder="mail@domain.com" class="form-control middle" required>
-            <input type="text"  name="mobile" id="mobile" placeholder="Mobile." class="form-control middle">
-            <canvas id="secret_img" width="110" height="25" style="border:1px solid #d3d3d3;padding-top: 8px; padding-left:5px;float: left;"><br /> Browser Not Support HTML5.</canvas><p style="font-size:12px;font-weight: bold;float: left;padding:10px 0 0 8px;">Not getting it ?<a href="javascript:void(0);" onclick="myJsMain.commonFunction.GeneratNewImage();">Click here</a>  to get a new.</p> 
+            <input type="text"  name="mobile" id="mobile" placeholder="Mobile." class="form-control middle">-->
           </div>
           <div class="col-md-6">
-              <input type="text"  name="contactNo" id="contactNo" placeholder="Contact No." class="form-control top" required>
-              <input type="text" name="fax" id="fax" placeholder="Fax Number" class="form-control middle" required>
-              <input type="text" name="address" id="address" placeholder="Address" class="form-control middle" required>
-              <input type="text" name="city" id="city" placeholder="City" class="form-control middle" required>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="contactNo" name="contactNo" type="phone" placeholder="" class="form-control " required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="fax" name="fax" type="phone" placeholder="enter your fax" class="form-control " >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="address" name="address" type="text" placeholder="enter your address" class="form-control " required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="city" name="city" type="text" placeholder="enter your city" class="form-control " required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+              
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
               <select name="countryId" id="countryId" class="form-control middle" required>
                   <option> * Select * </option>
                   <?php foreach($CountryDataArr AS $k){?>
                   <option value="<?php echo $k->countryId;?>"><?php echo $k->countryName;?></option>
                   <?php }?>
             </select>
-              <span class="stateIdSpan"><select name="stateId" id="stateId" class="form-control middle" required>
-            	<option value=""> * Select * </option>
-                
-            </select></span>
-            <input type="text" name="zip" placeholder="Zip Code / Postal Code" id="zip" class="form-control middle" required>
-            <input type="text" class="form-control bottom" placeholder="Security Code Shown on Left Side" name="secret" id="secret">
+                    </div>
+              </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8 stateIdSpan"> 
+                        <select name="stateId" id="stateId" class="form-control middle" required>
+                            <option value=""> * Select * </option>  
+                        </select>
+                    </div>
+              </div>
+              <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                      <input id="zip" name="zip" type="text" placeholder="enter your zip" class="form-control " required >
+                      <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+                </div>
+            <div class="form-group has-error">
+                    <div class="input-group col-md-8"> 
+                    <input type="text" class="form-control bottom" placeholder="Security Code Shown on Left Side" name="secret" id="secret">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    </div>
+            </div>
+            <!--<input type="text"  name="contactNo" id="contactNo" placeholder="Contact No." class="form-control top" required>
+              
+              <input type="text" name="fax" id="fax" placeholder="Fax Number" class="form-control middle" required>
+              <input type="text" name="address" id="address" placeholder="Address" class="form-control middle" required>
+              <input type="text" name="city" id="city" placeholder="City" class="form-control middle" required>
+              <input type="text" name="zip" placeholder="Zip Code / Postal Code" id="zip" class="form-control middle" required>-->
           </div>
-              <div class="col-md-12"><textarea name="aboutMe" id="aboutMe" placeholder="About Me" class="form-control"></textarea></div>
+              <div class="col-md-6"><textarea name="aboutMe" id="aboutMe" placeholder="About Me" class="form-control"></textarea></div>
           <div class="col-md-12"><p><input type="checkbox" value="1" class="required" id="agree" name="agree"> I agree to the <a style="text-decoration:underline;" href="javascript:void(0);">terms and conditions</a></p> </div>
           <div class="col-md-12"><p><input type="checkbox" value="1" id="receiveNewsLetter" name="receiveNewsLetter"> I want to receive newsletter updates</p></div>
               
