7b3cd75bb7bab547351844cdba6254881fdeec0c
kousik <kousik.das@outlook.com>
2015-10-29 19:26:49 +0530

    cart view update

--- application/views/cart.php
+++ application/views/cart.php
@@ -6,133 +6,123 @@
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <div class="cart-heading clearfix">
                         <h3>
-                            Shopping Cart <span>(1 Item)</span>
+                            Shopping Cart <span>(<?=$this->cart->total_items()?> Item<?php if($this->cart->total_items() > 1): echo 's';endif;?>)</span>
                         </h3>
                         
                     </div>
           </div>
-    
+<?php 
+//print_r($this->cart->contents());
+$cart = $this->cart->contents();
+$is_group = false;
+foreach($cart as $item):
+    if(isset($item['options']['orderType']) && $item['options']['orderType'] == 'GROUP'):
+        $is_group = true; continue;
+    endif;
+endforeach;
+?>
     
                     
-                    <div class="cart-container-table">
-                        <table id="cart" class="table table-hover table-condensed">
-                                        <thead>
-                                            <tr>
-                                                <th style="width:55%">Product</th>
-                                                <th style="width:12%">Price</th>
-                                                <th style="width:8%">Quantity</th>
-                                                <th style="width:15%" class="text-center">Subtotal</th>
-                                                <th style="width:10%" align="right"></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td data-th="Product">
-                                                    <div class="row">
-                                                        <div class="col-sm-3 product-img"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-                                                        <div class="col-sm-9 product-details">
-                                                            <h4 class="nomargin">Product 1</h4>
-                                                            <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td data-th="Price">$1.99</td>
-                                                
-                                                <td data-th="Quantity">
-                                                    <input type="number" class="form-control text-center" value="1">
-                                                </td>
-                                                <td data-th="Subtotal" class="text-center">1.99</td>
-                                                <td class="actions" data-th="" align="right">
-                                                    <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-                                                </td>
-                                            </tr>
-                                      
-                                        	<tr>
-                                                <td data-th="Product">
-                                                    <div class="row">
-                                                        <div class="col-sm-3 product-img"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-                                                        <div class="col-sm-9 product-details">
-                                                            <h4 class="nomargin">Product 1</h4>
-                                                            <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td data-th="Price">$1.99</td>
-                                                
-                                                <td data-th="Quantity">
-                                                    <input type="number" class="form-control text-center" value="1">
-                                                </td>
-                                                <td data-th="Subtotal" class="text-center">1.99</td>
-                                                <td class="actions" data-th="" align="right">
-                                                    <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-                                                </td>
-                                            </tr>
-                                            
-                                            <tr>
-                                                <td data-th="Product">
-                                                    <div class="row">
-                                                        <div class="col-sm-3 product-img"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-                                                        <div class="col-sm-9 product-details">
-                                                            <h4 class="nomargin">Product 1</h4>
-                                                            <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td data-th="Price">$1.99</td>
-                                                
-                                                <td data-th="Quantity">
-                                                    <input type="number" class="form-control text-center" value="1">
-                                                </td>
-                                                <td data-th="Subtotal" class="text-center">1.99</td>
-                                                <td class="actions" data-th="" align="right">
-                                                    <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-                                                </td>
-                                            </tr>
-                                        
-                                            <tr>
-                                                <!--<td colspan="5" valign="middle">
-                                                   <div class="pincode-check-enable row">
-                                                    <div class="col-sm-3 col-xs-12"><span class="pull-left pincode-TEXT">Check Availability at Pincode</span></div> 
-                                                    <div  class="col-sm-3 col-xs-12">
-                                                    <div class="input-group">
-                                                      <input type="text" class="form-control" aria-label="Text input with dropdown button" id="pincode-value">
-                                                      <div class="input-group-btn">
-                                                        <button type="button" class="btn btn-gray check-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Check </button>
-                                                        
-                                                      </div><!-- /btn-group -->
-                                                    <!--</div>
-                                                    
-                                                    </div>
-                                                    <!--<div id="pincode-error-tooltip" class="hidden">
-                                                        <p class="pincode-error-text">Please enter a valid pincode.</p>
-                                                    </div>-->
-                                                <!--</div>							
-                                                </td>-->
-                                            </tr>
-                                        </tbody>
-                                        
-                                        <tfoot>
-                                            
-                                            <!--<tr>
-                                                <td colspan="3">Delivery and payment options can be selected later</td>
-                                                <td class="text-center">Free</td>
-                                                <td></td>
-                                            </tr>-->
-                                            <tr class="visible-xs">
-                                                <td class="text-center"><strong>Total 1.99</strong></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
-                                                <td colspan="2" class="hidden-xs"></td>
-                                                <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
-                                                <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                    </div>
+        <div class="cart-container-table">
+            <table id="cart" class="table table-hover table-condensed">
+                <thead>
+                    <tr>
+                        <th style="width:55%">Product</th>
+                        <th style="width:12%">Price</th>
+                        <th style="width:8%">Quantity</th>
+                        <th style="width:15%" class="text-center">Subtotal</th>
+                        <th style="width:10%" align="right"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-th="Product">
+                            <div class="row">
+                                <div class="col-sm-3 product-img"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                                <div class="col-sm-9 product-details">
+                                    <h4 class="nomargin">Product 1</h4>
+                                    <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td data-th="Price">$1.99</td>
+
+                        <td data-th="Quantity">
+                            <input type="number" class="form-control text-center" value="1">
+                        </td>
+                        <td data-th="Subtotal" class="text-center">1.99</td>
+                        <td class="actions" data-th="" align="right">
+                            <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+                            <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+                        </td>
+                    </tr>
+                </tbody>
+
+                <tfoot>
+                    <tr class="visible-xs">
+                        <td class="text-center"><strong>Total 1.99</strong></td>
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                        <td colspan="2" class="hidden-xs"></td>
+                        <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+                        <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
+                    </tr>
+                </tfoot>
+            </table>
+            <?php if($is_group):?>
+            <h4>Group Order</h4>
+            <table id="cart" class="table table-hover table-condensed">
+                <thead>
+                    <tr>
+                        <th style="width:55%">Product</th>
+                        <th style="width:12%">Price</th>
+                        <th style="width:8%">Quantity</th>
+                        <th style="width:15%" class="text-center">Subtotal</th>
+                        <th style="width:10%" align="right"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                foreach($cart as $item):
+                    if(isset($item['options']['orderType']) && $item['options']['orderType'] == 'GROUP'):?>
+                    <tr>
+                        <td data-th="Product">
+                            <div class="row">
+                                <div class="col-sm-3 product-img"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                                <div class="col-sm-9 product-details">
+                                    <h4 class="nomargin"><?=$item['name']?></h4>
+                                </div>
+                            </div>
+                        </td>
+                        <td data-th="Price"><?=$item['price']?></td>
+
+                        <td data-th="Quantity">
+                            <?=$item['qty']?>
+                        </td>
+                        <td data-th="Subtotal" class="text-center"><?=$item['subtotal']?></td>
+                        <td class="actions" data-th="" align="right">
+                            <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+                            <button class="btn btn-success btn-sm">Checkout <i class="fa fa-angle-right"></i></button>
+                        </td>
+                    </tr>
+                    <?php endif;
+                    endforeach;?>
+                </tbody>
+
+                <tfoot>
+                    <tr class="visible-xs">
+                    </tr>
+                    <tr>
+                        <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                        <td colspan="2" class="hidden-xs"></td>
+                        <td class="hidden-xs text-center"></td>
+                        <td></td>
+                    </tr>
+                </tfoot>
+            </table>
+            <?php endif; ?>
+        </div>
                     
                     
     </div>
