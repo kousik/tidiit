7eb567d071372c421e01e55bfa9baa9ce94a2217
cto-tidiit <cto.tidiit@gmail.com>
2015-11-24 12:23:28 +0530

    changes in email tamplate and API

--- application/views/email_template/group_order_re_start.php
+++ application/views/email_template/group_order_re_start.php
@@ -6,11 +6,16 @@
     <div style="clear: both;width:100%; height: 40px"></div>
     <div style="clear: both;width:100%;text-align: left;">
         <table style="text-align: left;">
-            <tr><td>Hi, <br> You have requested to buy group order product.</td></tr>
+            <tr><td>Hi, <br /><br /> You have requested to buy group order product.</td></tr>
+            <tr><td>&nbsp;</td></tr>
             <tr><td>Product is <a href='#'><?php echo $group_order_re_start['TEMPLATE_GROUP_RE_ORDER_START_PRODUCT_TITLE'];?></a></td></tr>
+            <tr><td>&nbsp;</td></tr>
             <tr><td>Want to process the order ?</td></tr>
+            <tr><td>&nbsp;</td></tr>
             <tr><td><a href='<?php echo BASE_URL;?>shopping/group-order-decline/<?php echo base64_encode($group_order_re_start['TEMPLATE_GROUP_RE_ORDER_START_ORDER_ID1']*226201);?>'>Decline the Group Order</a></td></tr>
+            <tr><td>&nbsp;</td></tr>
             <tr><td><a href='<?php echo BASE_URL;?>shopping/group-re-order-accept-process/<?php echo base64_encode($group_order_re_start['TEMPLATE_GROUP_RE_ORDER_START_ORDER_ID1']*226201);?>'>Accept the Group Order</a></td></tr>
+            <tr><td>&nbsp;</td></tr>
             <tr><td>Thanks <br> Tidiit Team.</td></tr>
         </table>
     </div>
