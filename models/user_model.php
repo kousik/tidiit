3272d81269593d536a2244474f855bd41bf5e604
kousik <kousik.das@outlook.com>
2015-12-10 00:18:20 +0530

    shipping table code line updated

--- models/user_model.php
+++ models/user_model.php
@@ -241,7 +241,7 @@ class User_model extends CI_Model {
 
     function get_all_users_by_locality($localityId){
         $this->db->where('userId !=', $this->session->userdata('FE_SESSION_VAR'));
-        $this->db->select('userId')->from($this->_bill_address)->where('localityId',$localityId);
+        $this->db->select('userId')->from($this->_shipping_address)->where('localityId',$localityId);
         $query=$this->db->get();
         $data = $query->result();
         if($data): 
