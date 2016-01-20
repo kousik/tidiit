f110c8d5a0eafbf684052855c62e7d780671ed1d
cto-tidiit <cto.tidiit@gmail.com>
2016-01-20 23:16:25 +0530

    fix DOB foramt for app developer

--- app/application/controllers/appdata.php
+++ app/application/controllers/appdata.php
@@ -122,8 +122,9 @@ class Appdata extends REST_Controller {
         else:
             $userData=$this->user->get_details_by_id($userId,TRUE);
             $userDataDOBArr=  explode('-',$userData[0]['DOB']);
+            //pre($userDataDOBArr);die;
             if(count($userDataDOBArr)>0){
-                $result['userProfileDateData']=$userDataDOBArr[2].'-'.$userDataDOBArr[1].'-'.$userDataDOBArr[0];
+                $userData[0]['DOB']=$userDataDOBArr[2].'-'.$userDataDOBArr[1].'-'.$userDataDOBArr[0];
                 $result['userProfileData']=$userData;
             }else{
                 $result['userProfileData']=$userData;
