dc18378aabcd6169670932e73573d6cd83826ecb
cto-tidiit <cto.tidiit@gmail.com>
2016-01-24 11:31:06 +0530

    checking

--- models/user_model.php
+++ models/user_model.php
@@ -687,8 +687,8 @@ class User_model extends CI_Model {
         if($this->db->where('userId',$userId)->from($this->_user_product_type_category)->count_all_results()>0){
             /// update record
             $this->db->where('userId',$userId);
-            @mail('judhisahoo@gmail.com','update_user_product_type_category query string',$this->db->last_query());
             $this->db->update($this->_user_product_type_category,$dataArr);
+            @mail('judhisahoo@gmail.com','update_user_product_type_category query string',$this->db->last_query());
             return TRUE;
         }else{
             $dataArr['userId']=$userId;
