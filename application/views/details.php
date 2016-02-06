e41277e3b0f084c9b9d2f907051d63fc04c4d5cc
kousik <kousik.das@outlook.com>
2016-02-06 11:04:57 +0530

    latest backup  updated

--- application/views/details.php
+++ application/views/details.php
@@ -136,11 +136,56 @@ jQuery(document).ready(function(){
     });
 });  
 </script>
+<button class="inline btn btn-primary howItWork" data-toggle="modal" data-target=".bs-example-modal-lg" href="#inline_content">HOW IT WORKS</button>
+
+        
+<!-- This contains the hidden content for inline calls -->
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-lg opn_box" style='padding:10px; background:#fff;  border-radius:10px;'>
+        <button aria-label="Close" data-dismiss="modal" class="close" type="button"><span aria-hidden="true">×</span></button>
+        <div class="container">
+            <div class="banner">
+                <img src="<?php echo SiteImagesURL;?>works_img.jpg" class="img-responsive" />
+                <h4>A Better Way to Purchase For Your Business</h4>
+            </div>
+            <h2>HOW IT WORKS</h2>
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                        <div class="work_box">
+                        <img src="<?php echo SiteImagesURL;?>work_img.png" />
+                        <p>Tell us what you need and we will send your request to all relevant suppliers </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                        <div class="work_box">
+                        <img src="<?php echo SiteImagesURL;?>work_img1.png" />
+                        <p>Receive and compare multiple customized quotes within 48 hours </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                        <div class="work_box">
+                        <img src="<?php echo SiteImagesURL;?>work_img2.png" />
+                        <p>Negotiate and finalize the deal directly with the suppliers, knowing that you have made the right choice </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div id="autoLoadHowItWorks"></div>
-
-<?php echo $common_how_it_works;?>
-<?php echo $feedback;?>
-
+<a id="button1" class="quick_cntct" href="javascript:void(0)">FEEDBACK</a>
+        
+<div class="side_contact" id="t1">
+    <h3>Feedback</h3>
+    <form method="post" action="" name="#" id="#" class="feed_back">
+        <input type="text" class="required" placeholder="Enter Your Name" id="myname" name="myname">
+        <input type="text" class="required" placeholder="Enter Your Phone" id="myphone" name="myphone">
+        <input type="text" class="required email" placeholder="Enter Your Email Id" id="myemail" name="myemail">
+        <textarea class="required" placeholder="Enter Your Message" rows="" cols="" name="message"></textarea>
+        <input type="submit" value="Submit" name="sbmt">
+    </form>
+</div>
 <div class="container">
   <div class="comp-breadcrumbs">
     <div class="bread-crumb" id="breadCrumbWrapper"> 
