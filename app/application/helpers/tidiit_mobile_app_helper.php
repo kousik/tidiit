711122eb42b1e2607ffd8e6d6316882458fa4194
cto-tidiit <cto.tidiit@gmail.com>
2016-02-16 23:44:26 +0530

    fixed bugs

--- app/application/helpers/tidiit_mobile_app_helper.php
+++ app/application/helpers/tidiit_mobile_app_helper.php
@@ -292,7 +292,7 @@ if ( ! function_exists('send_sms_notification')):
      */
     $SMS_SEND_ALLOW=$CI->siteconfig->get_value_by_name('SMS_SEND_ALLOW');
     if($SMS_SEND_ALLOW=='yes'){
-        if($data['receiverMobileNumber']==""){
+        if(!array_key_exists('receiverMobileNumber', $data) || $data['receiverMobileNumber']==""){
             return FALSE;
         }else{
             //Send Mobile message
