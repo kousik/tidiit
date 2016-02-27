ddc5e0926ba615717ce31e551e2461625441c391
cto-tidiit <cto.tidiit@gmail.com>
2016-02-27 23:56:00 +0530

    checking

--- app/application/controllers/buying_club.php
+++ app/application/controllers/buying_club.php
@@ -16,7 +16,7 @@ class Buying_club extends REST_Controller {
     }
     
     function checking_user_data_post(){
-        $userId=$this->post($userId);
+        $userId=$this->post('userId');
         $user=$this->user->get_details_id($userId);
         pre($user);die;
     }
