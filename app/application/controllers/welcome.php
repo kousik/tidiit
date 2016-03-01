709edd47fdde10f247215b0b62770104754ea9f6
cto-tidiit <cto.tidiit@gmail.com>
2016-03-01 23:08:16 +0530

    testing api

--- app/application/controllers/welcome.php
+++ app/application/controllers/welcome.php
@@ -1,28 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-        public function __construct(){
-		parent::__construct();
-                $this->load->model('Admin_model');
-	}
-	
+require APPPATH.'/libraries/REST_Controller.php';
+class Welcome extends REST_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->model('User_model','user');
+    }
+    
+    function login_post(){
+        $userName=  $this->post('userName');
+        $password=  $this->post('password');
+        
+        $result=array();
+        $result['response']="true";
+        $result['postData']="$userName: ".$userName.' <=> $password= '.$password;
+        header('Content-type: application/json');
+        echo json_encode($result);
+    }
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
