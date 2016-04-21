59b2865f27e054cfdfba1b5597be4239daabb5c4
cto-tidiit <cto.tidiit@gmail.com>
2016-04-21 23:01:45 +0530

    update link for tidiit commission manageer from webamdin

--- application/views/webadmin/site_data_config_link.php
+++ application/views/webadmin/site_data_config_link.php
@@ -77,6 +77,9 @@
             <tr height="20" bgcolor="#DFDFDF">
               <td  style="padding-left:10px;"><img src="<?=$SiteImagesURL?>webadmin/arrow.gif" border="0" align="absmiddle"/>&nbsp;<a href="<?php echo base_url().'webadmin/help_topics/viewlist';?>">Help Topics manager</a> </td>
             </tr>
+            <tr height="20" bgcolor="#DFDFDF">
+              <td  style="padding-left:10px;"><img src="<?=$SiteImagesURL?>webadmin/arrow.gif" border="0" align="absmiddle"/>&nbsp;<a href="<?php echo base_url().'webadmin/tidiit_commission/viewlist';?>">Tidiit Commission manager</a> </td>
+            </tr>
   </table>
       
       </td>
