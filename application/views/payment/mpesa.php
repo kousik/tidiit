d1a2c464f7d49626ce532beccbb2a18e568d491c
cto-tidiit <cto.tidiit@gmail.com>
2016-07-12 12:45:47 +0530

    update function jcode code in mpesa view

--- application/views/payment/mpesa.php
+++ application/views/payment/mpesa.php
@@ -11,12 +11,6 @@
                     <div class="tab_dashbord">
                     	<div class="active row">
                             <div class="col-md-12 col-sm-12" style="height: 100px;">
-                                <script type="text/javascript">
-                                    jQuery(document).ready(function(){
-                                        myJsMain.commonFunction.showPleaseWait();
-                                        jQuery('#mpesaSubmitForm').submit();
-                                    });
-                                </script>
                                 <form name="mpesaSubmitForm" id="mpesaSubmitForm" action="<?php echo $checkOutURL;?>" method="post">
                                     <input type="hidden" name="gatewayparam" id="gatewayparam" value='<PaymentGatewayRequest><MCODE><?php echo $marchantCode;?></MCODE><TXNDATE><?php echo date('dmY');?></TXNDATE><TRANSREFNO><?php echo $orderIdStr.base64_encode('tidiit').time();?></TRANSREFNO><TXNTYPE>P</TXNTYPE><AMT>1.00</AMT><NARRATION>Test payment</NARRATION><RETURNURL><?php echo $returnURL; ?></RETURNURL><SURCHARGE>0</SURCHARGE><FILLER1></FILLER1><FILLER2></FILLER2><FILLER3></FILLER3><FILLER4></FILLER4><FILLER5></FILLER5></PaymentGatewayRequest>'/>
                                     <input type="hidden" name="checksum" id="checksum" value="<?php echo hash_hmac('sha256', '<PaymentGatewayRequest><MCODE>'.$marchantCode.'</MCODE><TXNDATE>'.date('dmY').'</TXNDATE><TRANSREFNO>'.$orderIdStr.base64_encode('tidiit').time().'</TRANSREFNO><TXNTYPE>P</TXNTYPE><AMT>1.00</AMT><NARRATION>Test payment</NARRATION><RETURNURL>'.$returnURL.'</RETURNURL><SURCHARGE>0</SURCHARGE><FILLER1></FILLER1><FILLER2></FILLER2><FILLER3></FILLER3><FILLER4></FILLER4><FILLER5></FILLER5></PaymentGatewayRequest>', 'sk2vmglp9f5s');?>"/>
@@ -30,4 +24,11 @@
       </div>
     </div>
   </div>
-</article><?php echo $footer;?>
+</article>
+<script type="text/javascript">
+jQuery(document).ready(function(){
+    myJsMain.commonFunction.showPleaseWait();
+    //jQuery('#mpesaSubmitForm').submit();
+    document.mpesaSubmitForm.submit();
+});
+</script><?php echo $footer;?>
