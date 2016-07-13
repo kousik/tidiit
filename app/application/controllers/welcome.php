19a43b01054ea8278776a917cecf626cd2ce2082
cto-tidiit <cto.tidiit@gmail.com>
2016-07-14 00:25:32 +0530

    checking soap api

--- app/application/controllers/welcome.php
+++ app/application/controllers/welcome.php
@@ -46,4 +46,35 @@ class Welcome extends REST_Controller {
             $this->response(array('error' => 'Your have not submited Multipart formate of formdata or file field name must be "userFile".'), 400);return FALSE;
         }
     }
+    
+    function soap_mpesa(){
+        ini_set('default_socket_timeout','300');
+        ini_set("soap.wsdl_cache_enabled", 0);
+        //$soapURL ='https://182.19.20.182:81/mcommerce/pgService?wsdl';
+        $soapURL ='http://182.19.20.182:81/mcommerce.webservices/pgService?wsdl';
+        echo '<pre>API URL : '.$soapURL.'<br>';
+        $soapParameters = Array('Userid' => "aJtlkG0NQTRBaLgVt4YC4A==", 'Password' => "7p/MAUl80KP+FdRERRyvlQ==") ;
+        echo 'Authentication Data <br>';print_r($soapParameters);
+        //$soapParameters = Array('login' => "aJtlkG0NQTRBaLgVt4YC4A==", 'password' => "7p/MAUl80KP+FdRERRyvlQ==") ;
+        $soapFunction = "processPayment" ;
+        echo 'soap method : '.$soapFunction.' <br>';
+        $soapFunctionParameters = Array('MCODE'=>'0021225252','TXNDATE' =>date('dmY'),'TRANSREFNO'=>time(),'MSISDN'=>'9556644964','AMT'=>'1.00','NARRATION'=>'shopping with mpes','mPIN'=>'1111'); ;
+        echo 'soap method parameter <br>';
+        print_r($soapFunctionParameters);
+        $soapClient = new SoapClient($soapURL, $soapParameters);
+        echo '<b>soap client init :</b> <br>';print_r($soapClient);
+        echo 'calling soap method and show result <br>';
+        $soapResult = $soapClient->__soapCall($soapFunction, $soapFunctionParameters) ;
+        print_r($soapResult);echo '</pre>';die;
+        //if(is_array($soapResult) && isset($soapResult['someFunctionResult'])) {
+        if(is_array($soapResult)) {
+            // Process result.
+            echo '<pre>';print_r($soapResult);die;
+        } else {
+            // Unexpected result
+            if(function_exists("debug_message")) {
+                debug_message("Unexpected soapResult for {$soapFunction}: ".print_r($soapResult, TRUE)) ;
+            }
+        } 
+    } 
 }
